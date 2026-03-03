955472c891f3de23abe3faccf062cb9f801dfff8
Ren1989lisa <renata.ortizchavez19@gmail.com>
2026-03-03 13:39:04 -0600

    Mover carpetas

---
+++ Practica Login y Protección de rutas/src/pages/Admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StripedRowExample from '../Componentes/StripedRowExample'
+
+const Admin = () => {
+  return (
+    <div>
+      <StripedRowExample/>
+    </div>
+  )
+}
+
+export default Admin
