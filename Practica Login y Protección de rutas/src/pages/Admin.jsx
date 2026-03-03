c39f8e6e747abba72658cd02c619e6646a591be8
Ren1989lisa <renata.ortizchavez19@gmail.com>
2026-03-03 14:21:36 -0600

    Tabla de admin

--- Practica Login y Protección de rutas/src/pages/Admin.jsx
+++ Practica Login y Protección de rutas/src/pages/Admin.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
-import StripedRowExample from '../Componentes/StripedRowExample'
+import { users } from "../data/users";
+import { Table, Container } from 'react-bootstrap';
 
 const Admin = () => {
   return (
-    <div>
-      <StripedRowExample/>
-    </div>
+    <Container className="mt-5">
+      <h2 className="mb-4">Administración</h2>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Usuario</th>
+            <th>Nombre</th>
+            <th>Rol</th>
+            <th>Contraseña</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{user.username}</td>
+              <td>{user.nombre}</td>
+              <td>{user.rol}</td>
+              <td>{user.password}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   )
 }
 
-export default Admin
+export default Admin;
