2a664a032af6f3ca6cec454be6355b9d02b9fa36
lum-i3 <dana.bahena.21@gmail.com>
2026-03-03 14:45:13 -0600

    Se agrego el componente boton, la pagina Login y se hicieron cambios en el main y app

--- Practica Login y Protección de rutas/src/main.jsx
+++ Practica Login y Protección de rutas/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-  <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
+
 )
