be18e9cbad7036c944b69599ce044bc1c97ea7a6
Robertin2102 <20243ds129@utez.edu.mx>
2026-03-03 23:41:34 -0600

    hice cambios en el diseño

--- Practica Login y Protección de rutas/src/main.jsx
+++ Practica Login y Protección de rutas/src/main.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
