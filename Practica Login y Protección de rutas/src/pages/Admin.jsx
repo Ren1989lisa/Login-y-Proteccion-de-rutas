1b2adc4a5d9ff4678d631b9e1c034a3bfa6f2189
alejandro rios <20243ds144@utez.edu.mx>
2026-03-03 22:46:35 -0600

    tabla reutilizable

--- Practica Login y Protección de rutas/src/pages/Admin.jsx
+++ Practica Login y Protección de rutas/src/pages/Admin.jsx
@@ -1,48 +1,27 @@
 import { users } from "../data/users";
-import { Table, Container } from 'react-bootstrap';
-import Boton from "../Componentes/Boton"
+import { Container } from "react-bootstrap";
+import Boton from "../Componentes/Boton";
+import TablaUsuarios from "../Componentes/TablaUsuarios";
 import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <>
-    <Container className="mt-5">
-      <h2 className="mb-4">Admin</h2>
+      <Container className="mt-5">
+        <h2 className="mb-4">Admin</h2>
 
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>Usuario</th>
-            <th>Nombre</th>
-            <th>Rol</th>
-            <th>Contraseña</th>
-          </tr>
-        </thead>
+        <TablaUsuarios users={users} />
+      </Container>
 
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{user.username}</td>
-              <td>{user.nombre}</td>
-              <td>{user.rol}</td>
-              <td>{user.password}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
-
-    <Boton
-      type="primary"
-      text="Regresar"
-      action={() => navigate("/profile")}
-    />
+      <Boton
+        type="primary"
+        text="Regresar"
+        action={() => navigate("/profile")}
+      />
     </>
-  )
-}
+  );
+};
 
-export default Admin;
+export default Admin;  
